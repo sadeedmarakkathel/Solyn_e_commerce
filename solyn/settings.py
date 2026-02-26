695097a1968332254162fcdca32497f6681f0918
sadeedmarakkathel <sadeed.official@gmail.com>
2026-02-26 11:14:45 +0530

    Make static storage conditional on DEBUG to prevent startup crashes

--- solyn/settings.py
+++ solyn/settings.py
@@ -93,7 +93,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+if not DEBUG:
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media files
 MEDIA_URL = 'media/'
